d4452432199fb7249f9d5f420629572567b3a5d3
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-04-15 09:59:02 -0300

    fix: fix eslint errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './global.css'
 
+import { Button } from './components/ui/button'
+
 export function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="flex h-screen items-center justify-center bg-zinc-900">
+      <Button variant={'secondary'}>Login</Button>
+    </div>
   )
 }
