dea48c8112d4e9a6bdd72c5e2198916fe0f8318f
Carlos Longhi <carloslonghi.cl@gmail.com>
2024-04-17 00:30:31 -0300

    feat: create page sign-in

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,36 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-      <h1>Sign In</h1>
+      <div className="p-6">
+        <div className="w-96 space-y-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar Painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Gerencie sua Pizzaria pelo nosso painel!
+            </p>
+          </div>
+
+          <form className="space-y-6">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" />
+            </div>
+
+            <Button className="w-full" type="submit">
+              Acessar painel
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
